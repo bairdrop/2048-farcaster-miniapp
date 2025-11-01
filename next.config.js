7c60b8d409de4481dcd26f1fa3d1d611a8d23648
bairdrop <116457570+bairdrop@users.noreply.github.com>
2025-11-01 17:59:19 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019a4041-3e02-5e3c-97b1-cc5156f56e02',
+        permanent: false,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
